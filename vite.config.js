6d2ba6779b11c84a08e028d6a42eaaa197400ec0
Gixbond <kevin.stirnimann@daktiv.ch>
2025-09-02 22:53:45 +0200

    Switch to Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/power-stage/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx']  // Explizit .jsx Unterstützung hinzufügen
